fd90099c40a05350107775cb8ea7106848d2560f
Alina <tyalinav@yandex.ru>
2023-09-26 13:05:30 +0300

    An array of user objects has been generated

---
+++ script.js
@@ -0,0 +1,51 @@
+// Массив объектов пользователей
+const users = [
+    {
+        name: "Марго",
+        surname: "Робби",
+        age: 33,
+        email: "margo_barbi@yandex.ru"
+    },
+    {
+        name: "Райан",
+        surname: "Гослинг",
+        age: 42,
+        email: "gosling@rambler.ru"
+    },
+    {
+        name: "Дуа",
+        surname: "Липа",
+        age: 28,
+        email: "DuaLipa@gmail.com"
+    },
+    {
+        name: "Хелен",
+        surname: "Миррен",
+        age: 78,
+        email: "mirren1945@yandex.ru"
+    },
+    {
+        name: "Кристофер",
+        surname: "Нолан",
+        age: 53,
+        email: "nolanStar@yandex.ru"
+    },
+    {
+        name: "Киллиан",
+        surname: "Мерфи",
+        age: 47,
+        email: "murphy@gmail.com"
+    },
+    {
+        name: "Флоренс",
+        surname: "Пью",
+        age: 27,
+        email: "Pugh@rambler.ru"
+    },
+    {
+        name: "Эмили",
+        surname: "Блант",
+        age: 40,
+        email: "EmilyBlunt@rambler.ru"
+    },
+];
